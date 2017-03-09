d4594003ea6a19b512ca2cda3f0c1a94bbf19882
chenli <chenli90s@live.com>
2017-03-09 09:21:15 +0800

    0.2 this verison compelet annouce and comment

---
+++ src/service/AnnounceService.java
@@ -0,0 +1,37 @@
+package service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.List;
+
+import entity.Announce;
+import entity.User;
+
+
+public interface AnnounceService{
+
+
+    public int insert(Announce pojo);
+
+    public int insertSelective(Announce pojo);
+
+    public int insertList(List<Announce> pojos);
+
+    public int update(Announce pojo);
+
+    public int insertAnnounce(Announce pojo);
+
+    public List<Announce> selectAnnounce();
+
+    public Announce selectAnnounceAndcomments(String id);
+
+    public void addAnnounce(User user, Announce announce);
+
+    int updateHotById(String aId);
+
+    String getUserAnnounce(JsonNode jsonNode,String id);
+
+    String dispacherAnnounce(Announce announceobj);
+
+    String delectAnnounce(String aid);
+}
